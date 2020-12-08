ac355ef05831cbe7d4f4410b1e13dc151cff27d1
Mr-Sena <jsenna985@gmail.com>
2020-12-08 13:38:44 -0300

    Métodos CRUD

--- Model/ProjetoIntegrador/src/main/java/com/NetOngs/NetworkFeminino/ModelTema.java
+++ Model/ProjetoIntegrador/src/main/java/com/NetOngs/NetworkFeminino/ModelTema.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "Tema")
@@ -20,10 +21,46 @@ public class ModelTema {
 	private String categoria;
 	
 	@Column
+	@NotNull
 	private String nome;
 	
 	@Column
 	private int classificacaoIndicativa;
+
+	public Long getId_tema() {
+		return id_tema;
+	}
+
+	public void setId_tema(Long id_tema) {
+		this.id_tema = id_tema;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getClassificacaoIndicativa() {
+		return classificacaoIndicativa;
+	}
+
+	public void setClassificacaoIndicativa(int classificacaoIndicativa) {
+		this.classificacaoIndicativa = classificacaoIndicativa;
+	}
+	
+	//Getters and Setters
+	
 	
 
 }
