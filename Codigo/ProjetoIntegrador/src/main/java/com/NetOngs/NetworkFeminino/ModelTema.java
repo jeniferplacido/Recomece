1360ff6dc39a547ab047117a8bdcf41fe588d8bc
Mr-Sena <jsenna985@gmail.com>
2020-12-11 13:41:07 -0300

    Versão executável

--- Codigo/ProjetoIntegrador/src/main/java/com/NetOngs/NetworkFeminino/ModelTema.java
+++ Codigo/ProjetoIntegrador/src/main/java/com/NetOngs/NetworkFeminino/ModelTema.java
@@ -58,12 +58,5 @@ public class ModelTema {
 	public void setClassificacaoIndicativa(int classificacaoIndicativa) {
 		this.classificacaoIndicativa = classificacaoIndicativa;
 	}
-	
-<<<<<<< HEAD:Model/ProjetoIntegrador/src/main/java/com/NetOngs/NetworkFeminino/ModelTema.java
-=======
 	//Getters and Setters
-	
-	
->>>>>>> ae6b969c06b266c1458a21c67fa634f03e07b583:Code/ProjetoIntegrador/src/main/java/com/NetOngs/NetworkFeminino/ModelTema.java
-
 }
