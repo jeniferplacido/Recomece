7bfbfa092e81a50ba4c1cc59108aec4d076dfb86
Mr-Sena <jsenna985@gmail.com>
2021-01-22 19:13:39 -0300

    Implementação da camada de segurança

---
+++ Desenvolvimento/Back-End/Recomece/src/main/java/RedeOrganizacional/Recomece/model/UserLogin.java
@@ -0,0 +1,47 @@
+package RedeOrganizacional.Recomece.model;
+
+public class UserLogin {
+
+	private String nome;
+	
+	private String userName;
+	
+	private String senha;
+	
+	private String toker;
+
+	
+	
+	//Getters and Setters
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getToker() {
+		return toker;
+	}
+
+	public void setToker(String toker) {
+		this.toker = toker;
+	}
+}
