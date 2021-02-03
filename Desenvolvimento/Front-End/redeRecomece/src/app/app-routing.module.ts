ecf94f55d42c4fd104e9ee77a0698c2f5d98f811
Mr-Sena <jsenna985@gmail.com>
2021-02-02 23:06:12 -0300

    Funções de postagem, direcionamento para rota tema e quadra de postagens

--- Desenvolvimento/Front-End/redeRecomece/src/app/app-routing.module.ts
+++ Desenvolvimento/Front-End/redeRecomece/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 import { PostagemComponent } from './postagem/postagem.component';
+import { TemaComponent } from './tema/tema.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'menu', pathMatch: 'full'},
   {path: 'menu', component: MenuComponent},
+  {path: 'tema', component: TemaComponent},
 
   {path: 'postagem', component: PostagemComponent}
 ];
--- Desenvolvimento/Front-End/redeRecomece/src/app/postagem/postagem.component.ts
+++ Desenvolvimento/Front-End/redeRecomece/src/app/postagem/postagem.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-postagem',
@@ -7,9 +15,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostagemComponent implements OnInit {
 
-  constructor() { }
+  postagem: Postagem = new Postagem()
+  listaPostagens: Postagem[]
 
-  ngOnInit(): void {
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+  idTema: number
+
+  user: User = new User()
+  idUser = environment.id
+
+
+  constructor(
+    private router: Router,
+    private postagemService: PostagemService,
+    private temaService: TemaService,
+    private authService: AuthService
+
+
+  ) { }
+
+  ngOnInit() {
+
+    if(environment.token == '') {
+      //alert('Sua seção expirou. Faça login novamente')
+      this.router.navigate( ['/menu'] )
+    }
+
+    this.getAllTema()
+    this.getAllPostagens()
+    this.findByIdUser()
+  }
+
+  getAllTema() {
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp
+    })
+  }
+
+  findByIdTema(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema) =>{
+      this.tema = resp
+    })
+  }
+
+  getAllPostagens() {
+    this.postagemService.getAllPostagens().subscribe((resp: Postagem[]) =>{
+      this.listaPostagens = resp
+    })
+  }
+
+
+  findByIdUser() {
+    this.postagemService.getByIdUser(this.idUser).subscribe((resp: User) => {
+      this.user = resp
+    })
+
+  }
+
+
+  publicar(){
+    this.tema.idTema = this.idTema
+    this.postagem.tema = this.tema
+
+    this.user.idUsuario = this.idUser
+    this.postagem.usuario = this.user
+
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem) => {
+      this.postagem = resp
+      this.postagem = new Postagem()
+      this.getAllTema()
+      alert('Postagem realizada com sucesso!')
+    })
+
   }
 
 }
